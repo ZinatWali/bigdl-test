83ae5b718de9c574b7d68e0755026f466098a9a7
Zinat Wali <zinat.wali@gmail.com>
2018-01-25 14:36:35 +0000

    Predict with simple NN

--- build.sbt
+++ build.sbt
@@ -7,6 +7,8 @@ scalaVersion := "2.11.11"
 libraryDependencies ++= Seq(
   "com.intel.analytics.bigdl" % "bigdl-SPARK_2.1" % "0.4.0",
   "org.apache.spark" % "spark-core_2.11" % "2.1.1",
-  "org.apache.spark" % "spark-sql_2.11" % "2.1.1"
+  "org.apache.spark" % "spark-sql_2.11" % "2.1.1",
+  "org.apache.spark" % "spark-mllib_2.11" % "2.1.1"
+
 )
 
--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,10 +1,18 @@
 package org.simple.stuff
 
-import com.intel.analytics.bigdl.utils.Engine
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.{Column, SQLContext}
+import breeze.linalg.DenseVector
+import com.intel.analytics.bigdl.Module
+import com.intel.analytics.bigdl.dataset.Sample
+import com.intel.analytics.bigdl.nn._
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.{Engine, T}
 import org.apache.spark.sql.functions._
-
+import org.apache.spark.sql.{Column, Row, SparkSession}
+import com.intel.analytics.bigdl._
+import com.intel.analytics.bigdl.models.lenet.LeNet5
+import com.intel.analytics.bigdl.optim._
+import org.apache.spark.rdd.RDD
 
 object TestRef{
   def main(args: Array[String])  : Unit = {
@@ -14,30 +22,25 @@ object TestRef{
       .createSparkConf()
       .setAppName("bigdl-blues")
       .setMaster("local[1]")
-    val sc = new SparkContext(conf)
-    val sqlc = new SQLContext(sc)
+    val ss = SparkSession.builder().config(conf).getOrCreate()
+    import ss.implicits._
+
+
     Engine.init
 
-    //  reading csv - rdd way
-    //  val csv = sc.textFile("creditcard.csv")
-    //  val headers = csv.first()
-    //  println(s"Column names: $headers")
-
-
-    val csv = sqlc
+    val csv = ss
       .read
       .format("com.databricks.spark.csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .csv("creditcard.csv")
+//      .limit(10000)
+      .cache()
 
 //    csv.printSchema()
 
-
     val colsToRemove = Seq("V28", "V27", "V26", "V25", "V24", "V23", "V22", "V20", "V15", "V13", "V8")
     val leanCSV = csv.select(csv.columns.filter(colName => !colsToRemove.contains(colName)).map(colName => new Column(colName)):_*)
-
-    leanCSV.printSchema()
 
     val enrichedCSV = leanCSV
       .withColumn("V1_", when(col("V1") < -3, 1).otherwise(0))
@@ -57,7 +60,8 @@ object TestRef{
       .withColumn("V18_", when(col("V18") < -2, 1).otherwise(0))
       .withColumn("V19_", when(col("V19") > 1.5 -3, 1).otherwise(0))
       .withColumn("V21_", when(col("V21") > 0.6, 1).otherwise(0))
-      .withColumn("Normal", when(col("Class") === 0, 1).otherwise(1))
+      .withColumn("Class", when(col("Class") === 0, 1).otherwise(2))
+//      .withColumn("Normal", when(col("Class") === 0, 1).otherwise(1))
       .withColumnRenamed("Class", "Fraud")
 
     val fraud = enrichedCSV.filter("Fraud == 1").count()
@@ -66,30 +70,148 @@ object TestRef{
     println(s"Fraud count: $fraud")
     println(s"Normal count: $normal")
 
-    val Array(xTrainOrig, xTestOrig) = enrichedCSV.randomSplit(Array(0.8, 0.2))
+    var Array(xTrain, xTest) = enrichedCSV.randomSplit(Array(0.8, 0.2))
 
-    val xTrainInterim = xTrainOrig.orderBy(rand)
-    val xTestInterim = xTestOrig.orderBy(rand)
-
-    val yTrain = xTrainInterim.select("Fraud", "Normal")
-    val yTest = xTestInterim.select("Fraud","Normal")
-
-    var xTrain = xTrainInterim.select(xTrainInterim.columns.filter(x => x != "Fraud" && x != "Normal").map(colName => new Column(colName)):_*)
-    var xTest = xTestInterim.select(xTestInterim.columns.filter(x => x != "Fraud" && x != "Normal").map(colName => new Column(colName)):_*)
-
-    println("xTrain Count: " + xTrain.count() + "xTest Count: " + xTest.count() + "yTrain Count: " + yTrain.count() + "yTest Count: " + yTest.count())
-
-
-//    val features = xTrain.columns
-//    for(x <- features)
-//    {
-//      val meanFeat = mean(leanCSV.select(x))
-//      val stdFeat = stddev(leanCSV.select(x))
+//    val xTrainInterim = xTrainOrig.orderBy(rand)
+//    val xTestInterim = xTestOrig.orderBy(rand)
 //
-//      xTrain = xTrain.withColumn(x, (col(x) - meanFeat) / stdFeat)
-//      xTest = xTest.withColumn(x, (col(x) - meanFeat) / stdFeat)
-//    }
+//    var xTrain = xTrainInterim
+//    var xTest = xTestInterim
 
-//    enrichedCSV.select("V1", "V1_").where(col("V1_") === 1).take(15).foreach(println)
+//    xTrain.printSchema()
+
+
+    val labels = Array("Fraud")
+    val features = xTrain.columns.filterNot(labels.contains(_))
+
+
+    for(x <- features)
+    {
+      if(x != "Fraud" && x != "Normal") {
+        val meanFeat = enrichedCSV.select(avg(x)).first().get(0)
+        val stdFeat = enrichedCSV.select(stddev(x)).first().get(0)
+
+//        println(meanFeat)
+
+        xTrain = xTrain.withColumn(x, (col(x) - meanFeat) / stdFeat)
+        xTest = xTest.withColumn(x, (col(x) - meanFeat) / stdFeat)
+      }
+    }
+
+    val xTrainFraud = xTrain.filter(row => row.getAs[Int]("Fraud") == 1)
+    List(1 to 100).foreach(_ => xTrain = xTrain.union(xTrainFraud))
+    xTrain = xTrain.orderBy(rand)
+    xTest = xTest.orderBy(rand)
+
+    val split = (xTest.count() / 2).toInt
+    val inputXTest = xTest.limit(split)
+    val inputXValid = xTest.except(inputXTest)
+
+    val samples : RDD[Sample[Float]]  = xTrain.rdd
+      .mapPartitions((rows: Iterator[Row]) => {
+        rows.map(row => {
+          val feats = Tensor(DenseVector(features.map(c => row.getAs[Double](c).toFloat)))
+          val labs = Tensor(DenseVector(labels.map(c => row.getAs[Int](c).toFloat)))
+          Sample(feats, labs)
+        })
+    })
+
+    val testRDD = inputXTest.rdd
+      .mapPartitions((rows: Iterator[Row]) => {
+        rows.map(row => {
+          val feats = Tensor(DenseVector(features.map(c => row.getAs[Double](c).toFloat)))
+          val labs = Tensor(DenseVector(labels.map(c => row.getAs[Int](c).toFloat)))
+          Sample(feats, labs)
+        })
+      })
+
+    val valRDD = inputXValid.rdd
+      .mapPartitions((rows: Iterator[Row]) => {
+        rows.map(row => {
+          val feats = Tensor(DenseVector(features.map(c => row.getAs[Double](c).toFloat)))
+          val labs = Tensor(DenseVector(labels.map(c => row.getAs[Int](c).toFloat)))
+          Sample(feats, labs)
+        })
+      })
+
+
+    val input_nodes = 36
+
+////  Multiplier maintains a fixed ratio of nodes between each layer.
+    val multiplier = 1.5
+
+////  Number of nodes in each hidden layer
+    val hidden_nodes1 = 72
+    val hidden_nodes2 = (hidden_nodes1 * multiplier).toInt
+    val hidden_nodes3 = (hidden_nodes2 * multiplier).toInt
+    val hidden_nodes4 = (hidden_nodes3 * multiplier).toInt
+
+    implicit val ev = TensorNumeric.NumericFloat
+    val model = Sequential[Float]()
+    model
+      .add(Linear(input_nodes,hidden_nodes1))
+      .add(Linear(hidden_nodes1, hidden_nodes2))
+      .add(Linear(hidden_nodes2, hidden_nodes3))
+      .add(Linear(hidden_nodes3, hidden_nodes4))
+      .add(GaussianDropout(0.5))
+      .add(Linear(hidden_nodes4, 2))
+      .add(SoftMax())
+
+//    model.add(Reshape(Array(1, 6, 6)))
+//      .add(SpatialConvolution(1, 6, 5, 5))
+//      .add(Tanh())
+//      .add(SpatialMaxPooling(2, 2, 2, 2))
+//      .add(Tanh())
+//      .add(SpatialConvolution(6, 12, 5, 5))
+//      .add(SpatialMaxPooling(2, 2, 2, 2))
+//      .add(Reshape(Array(12 * 4 * 4)))
+//      .add(Linear(12 * 4 * 4, 100))
+//      .add(Tanh())
+//      .add(Linear(100, 2))
+//      .add(LogSoftMax())
+
+    val training_epochs = 10
+    val training_dropout = 0.9
+    val display_step = 1
+    val batch_size = 2048
+    val learning_rate = 0.005
+
+    val optimizer = Optimizer[Float](
+      model=model,
+      sampleRDD=samples,
+      criterion= MSECriterion[Float](),
+      batchSize = batch_size, featurePaddingParam = null, labelPaddingParam = null
+    )
+
+    val trainedModel = optimizer
+      .setValidation(
+        trigger = Trigger.everyEpoch,
+        sampleRDD=valRDD,
+        vMethods = Array(new Top1Accuracy),
+        batchSize=batch_size)
+      .setOptimMethod(new Adagrad(learningRate=learning_rate, learningRateDecay=0.0002))
+      .setEndWhen(Trigger.maxEpoch(training_epochs))
+      .optimize()
+
+    val f = samples.filter(x => x.label() == Tensor(T(2f)))
+    val nf = samples.filter(x => x.label() == Tensor(T(1f)))
+    val toPredict = ss.sparkContext.parallelize(f.take(10).union(nf.take(10)))
+    println(s"Total frauds in input ${toPredict.count()}")
+
+    val predictResults = trainedModel.predictClass(toPredict)
+//    val ResultsZero = predictResult.filter(i => i == Tensor(T(0f))).count()
+//    val p = predictResult.take(50)
+
+    println("samples")
+
+    toPredict.take(50).foreach(x => {
+      println(x.label())
+    })
+
+    println(s"predict predict:")
+
+    for(i <- predictResults){
+      println(i)
+    }
   }
 }
